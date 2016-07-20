8097ada42ef1b213c1fee60788ab83fe8de47a1b
Pavel Pisarev <shustr8@gmail.com>
2016-07-20 12:23:07 +0300

    refactoring - unused code deleted

--- magento-blog/Atsumoriso/Blog/Block/Posts.php
+++ magento-blog/Atsumoriso/Blog/Block/Posts.php
@@ -39,24 +39,5 @@ class Atsumoriso_Blog_Block_Posts extends Mage_Core_Block_Template
 
      }
 
-    public function getOnePost($params)
-    {
-        $params = $this->getRequest()->getParams();
-        $coll = Mage::getModel('blog/post')->getCollection();
-
-        $fn = Mage::getModel('eav/entity_attribute')->loadByCode('1', 'firstname');
-        $ln = Mage::getModel('eav/entity_attribute')->loadByCode('1', 'lastname');
-
-        $coll->getSelect()
-            ->join(array('ce1' => 'customer_entity_varchar'), 'ce1.entity_id=main_table.`author_id`', array('firstname' => 'value'))
-            ->where('ce1.attribute_id='.$fn->getAttributeId())
-            ->join(array('ce2' => 'customer_entity_varchar'), 'ce2.entity_id=main_table.`author_id`', array('lastname' => 'value'))
-            ->where('ce2.attribute_id='.$ln->getAttributeId())
-            ->columns(new Zend_Db_Expr("CONCAT(`ce1`.`value`, ' ',`ce2`.`value`) AS fullname"))
-            ->where('ce2.entity_id=' . $params['id']);
-
-        return $coll->getData();
-
-    }
 
 }
