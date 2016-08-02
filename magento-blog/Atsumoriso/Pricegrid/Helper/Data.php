36bfdc799c8215ada4d69897f821c3c306ddeb42
Pavel Pisarev <shustr8@gmail.com>
2016-08-02 13:44:38 +0300

    Open-Closed principle

--- magento-blog/Atsumoriso/Pricegrid/Helper/Data.php
+++ magento-blog/Atsumoriso/Pricegrid/Helper/Data.php
@@ -2,5 +2,54 @@
 
 class Atsumoriso_Pricegrid_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    public function cleanDigitInput($value)
+    {
+        $value             = str_replace(",", ".", $value);
+        $value             = (float)abs($value);
+        return $value;
+    }
+
+    public function validateValueForMultiplication($value)
+    {
+        if($value < 0.01)
+            return false;
+        else
+            return true;
+    }
+
+    public function performOperationSubstruction($price, $value)
+    {
+        if ($value > $price) {
+            $newPrice = 0;
+        } else {
+            $newPrice = $price - $value;
+        }
+        return $newPrice;
+    }
+
+    public function performOperationAddition($price, $value)
+    {
+        return $price + $value;
+    }
+
+    public function performOperationAddPercent($price, $value)
+    {
+        return $price + ($price * $value) / 100;
+    }
+
+    public function performOperationSubstructPercent($price, $value)
+    {
+        if($value >100){
+            $newPrice = 0;
+        } else {
+            $newPrice = $price - ($price * $value) / 100;
+        }
+        return $newPrice;
+    }
+
+    public function performOperationMultiplication($price, $value)
+    {
+        return $price * $value;
+    }
 
 }
--- magento-blog/Atsumoriso/Pricegrid/controllers/Adminhtml/PriceController.php
+++ magento-blog/Atsumoriso/Pricegrid/controllers/Adminhtml/PriceController.php
@@ -13,10 +13,11 @@ class Atsumoriso_Pricegrid_Adminhtml_PriceController extends Mage_Adminhtml_Cont
         $operation         = $params['operation'];
         $value             = $params['value'];
 
-        $value             = str_replace(",", ".", $value);
-        $value             = (float)abs($value);
+        $value = Mage::helper('atsumoriso_pricegrid')->cleanDigitInput($value);
 
-        if ($operation == Atsumoriso_Pricegrid_Model_Observer::OPERATION_MULTIPLICATION && $value == 0){
+        if ($operation == Atsumoriso_Pricegrid_Model_Observer::OPERATION_MULTIPLICATION
+            && Mage::helper('atsumoriso_pricegrid')->validateValueForMultiplication($value) == true){
+
             Mage::getSingleton('adminhtml/session')->addError(Mage::helper('atsumoriso_pricegrid')->__('Operation is not allowed. '));
 
         } else {
@@ -24,7 +25,7 @@ class Atsumoriso_Pricegrid_Adminhtml_PriceController extends Mage_Adminhtml_Cont
                 ->addAttributeToFilter('entity_id', ['in' => $productsIdsArray])
                 ->addAttributeToSelect('price');
 
-            $productCollection = $this->_getNewPrice($productCollection, $operation, $value);
+            $productCollection = $this->_setNewPriceToCollection($productCollection, $operation, $value);
             $productCollection->save();
 
             Mage::getSingleton('adminhtml/session')->addSuccess(
@@ -34,40 +35,33 @@ class Atsumoriso_Pricegrid_Adminhtml_PriceController extends Mage_Adminhtml_Cont
             );
         }
 
-
-
         $this->_redirect('adminhtml/catalog_product');
-
     }
 
-    protected function _getNewPrice($productCollection, $operation, $value)
+    protected function _setNewPriceToCollection($productCollection, $operation, $value)
     {
         foreach ($productCollection as $product) {
             $price = $product->getPrice();
 
             switch ($operation) {
                 case Atsumoriso_Pricegrid_Model_Observer::OPERATION_ADDITION:
-                    $newPrice = $price + $value;
+                    $newPrice = Mage::helper('atsumoriso_pricegrid')->performOperationAddition($price, $value);
                     break;
 
                 case Atsumoriso_Pricegrid_Model_Observer::OPERATION_SUBTRACTION:
-                    if ($value > $price) {
-                        $newPrice = 0.01;
-                    } else {
-                        $newPrice = $price - $value;
-                    }
+                    $newPrice = Mage::helper('atsumoriso_pricegrid')->performOperationSubstruction($price, $value);
                     break;
 
                 case Atsumoriso_Pricegrid_Model_Observer::OPERATION_ADD_PERCENT:
-                    $newPrice = $price + ($price * $value) / 100;
+                    $newPrice = Mage::helper('atsumoriso_pricegrid')->performOperationAddPercent($price, $value);
                     break;
 
                 case Atsumoriso_Pricegrid_Model_Observer::OPERATION_SUBSTRUCT_PERCENT:
-                    $newPrice = $price - ($price * $value) / 100;
+                    $newPrice = Mage::helper('atsumoriso_pricegrid')->performOperationSubstructPercent($price, $value);
                     break;
 
                 case Atsumoriso_Pricegrid_Model_Observer::OPERATION_MULTIPLICATION:
-                    $newPrice = $price * $value;
+                    $newPrice = Mage::helper('atsumoriso_pricegrid')->performOperationMultiplication($price, $value);
                     break;
             }
 
